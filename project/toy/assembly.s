e04add6d43d7e5b34d13310aa8e27fbcd7d5d772
mpolague <mpolague@miners.utep.edu>
2020-04-22 10:06:43 -0600

    added assembly.s

--- project/toy/assembly.s
+++ project/toy/assembly.s
@@ -1,38 +1,38 @@
-	.arch mps430g2553
+	.arch mps430g2553 
 	.p2align 1,0
 
 	.data
 state:
-	.word 0
+	.word 0 //state starts on 0
 
 	.text
-jt:
+jt: //this is the jump table
 	.word out
 	.word option1
 	.word option2
 	.word option3
 	.word option4
 
-	.global advance
-advance:cmp #5,state
-	jnc out
-	mov &state, r12
-	add r12,r12
-	mov jt(r12), r0
+	.global advance //name of function 
+advance:cmp #5,state //checking if input is within the options
+	jnc out //jump if not carry, jump if it is not within the range
+	mov &state, r12 //state to reg
+	add r12,r12 //adds them to align to correct addresses 
+	mov jt(r12), r0 //uses jt as an offset for r12
 
-option1:pushl song1
-	call #buzzer_set_period
-	mov #0,&state
-	jmp out
+option1:pushl song1 //pushes song1 to the stack
+	call #buzzer_set_period //calls function
+	mov #0,&state //set state = 0
+	jmp out //jumps out
 
-option2:call #_state_advance
-	jmp out
+option2:call #_state_advance //calls function
+	jmp out //jumps out
 
-option3:call #buzzer_off
-	jmp out
+option3:call #buzzer_off //calls function
+	jmp out //jumps out
 
-option4:pushl song2
-	call #buzzer_set_period
-	jmp out
+option4:pushl song2 //pushes song2 onto stack
+	call #buzzer_set_period //calls function
+	jmp out //jumps out
 
-out:	pop r0
+out:	pop r0 //this is like a break statement
